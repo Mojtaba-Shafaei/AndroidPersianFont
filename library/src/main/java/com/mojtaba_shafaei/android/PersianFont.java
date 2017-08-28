1f52062f9af136a869fb56bd71bcfe3740c3cffa
mojtaba <shafaei.saeid@gmail.com>
2017-08-28 15:33:36 +0430

    1.0.3

--- library/src/main/java/com/mojtaba_shafaei/android/PersianFont.java
+++ library/src/main/java/com/mojtaba_shafaei/android/PersianFont.java
@@ -2,7 +2,12 @@ package com.mojtaba_shafaei.android;
 
 import android.content.Context;
 import android.graphics.Typeface;
+import android.support.annotation.IntDef;
 import android.support.v4.util.ArrayMap;
+
+import java.lang.annotation.Documented;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
 
 /**
  * Created by mojtaba on 8/22/17.
@@ -11,7 +16,7 @@ import android.support.v4.util.ArrayMap;
 public class PersianFont {
     private final static ArrayMap<Integer, Typeface> cache = new ArrayMap<>();
 
-    public static final int IRANSANS_ULTRA_LIGHT = 1;
+    public static final int IRAN_SANS_ULTRA_LIGHT = 1;
     public static final int IRANSANS_LIGHT = 2;
     public static final int IRANSANS_NORMAL = 3;
     public static final int IRANSANS_MEDIUM = 4;
@@ -23,13 +28,19 @@ public class PersianFont {
 
 
     ////////////////////////////////////
+    @Retention(RetentionPolicy.SOURCE)
+    @Documented
+    @IntDef({IRAN_SANS_ULTRA_LIGHT, IRANSANS_LIGHT, IRANSANS_NORMAL, IRANSANS_MEDIUM, IRANSANS_BOLD
+            , YEKAN_REGULAR, YEKAN_LIGHT, YEKAN_BOLD})
+    public @interface FontEnum {
+    }
     ////////////////////////////////////
 
     public static Typeface IRAN_SANS_ULTRA_LIGHT(Context context) {
-        if (cache.containsKey(IRANSANS_ULTRA_LIGHT)) {
-            cache.put(IRANSANS_ULTRA_LIGHT, Typeface.createFromAsset(context.getAssets(), "fonts/" + "IRANSansMobile_UltraLight.ttf"));
+        if (cache.containsKey(IRAN_SANS_ULTRA_LIGHT)) {
+            cache.put(IRAN_SANS_ULTRA_LIGHT, Typeface.createFromAsset(context.getAssets(), "fonts/" + "IRANSansMobile_UltraLight.ttf"));
         }
-        return cache.get(IRANSANS_ULTRA_LIGHT);
+        return cache.get(IRAN_SANS_ULTRA_LIGHT);
     }
 
     public static Typeface IRANSANS_LIGHT(Context context) {
@@ -79,5 +90,36 @@ public class PersianFont {
             cache.put(YEKAN_BOLD, Typeface.createFromAsset(context.getAssets(), "fonts/" + "IRANYekanMobileBold.ttf"));
         }
         return cache.get(YEKAN_BOLD);
+    }
+
+    public static Typeface get(Context context, @FontEnum int type) {
+        switch (type) {
+            case IRAN_SANS_ULTRA_LIGHT:
+                return IRAN_SANS_ULTRA_LIGHT(context);
+
+            case IRANSANS_LIGHT:
+                return IRANSANS_LIGHT(context);
+
+            case IRANSANS_NORMAL:
+                return IRANSANS_NORMAL(context);
+
+            case IRANSANS_MEDIUM:
+                return IRANSANS_MEDIUM(context);
+
+            case IRANSANS_BOLD:
+                return IRANSANS_BOLD(context);
+
+            case YEKAN_REGULAR:
+                return YEKAN_REGULAR(context);
+
+            case YEKAN_LIGHT:
+                return YEKAN_LIGHT(context);
+
+            case YEKAN_BOLD:
+                return YEKAN_BOLD(context);
+
+            default:
+                return IRANSANS_NORMAL(context);
+        }
     }
 }
